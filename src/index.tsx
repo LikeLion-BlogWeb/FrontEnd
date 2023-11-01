70230c49d434122949a56ae45ffe1605d8cbfd53
PANdas <krsy0411@naver.com>
2023-11-01 13:18:57 +0900

    초기 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
+// react
 import React from 'react';
+// reactDOM
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+// https://github.com/jen-frontend/fastcampus-react-blog
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
