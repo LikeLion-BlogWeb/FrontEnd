3809c332ac2c9ea79f4e79b26ae0cee6eb204bfa
PANdas <krsy0411@naver.com>
2023-11-03 16:18:17 +0900

    유틸 분리

---
+++ src/util/img.js
@@ -0,0 +1,5 @@
+export const IMG_SOURCE = {
+    img1: "https://unsplash.com/ko/%EC%82%AC%EC%A7%84/%EC%9E%90%ED%8C%90%EA%B8%B0%EB%A5%BC-%EC%A7%80%EB%82%98%EC%B3%90-%EA%B1%B7%EB%8A%94-%EB%82%A8%EC%9E%90-7Wbx2HNAGoo",
+    img2: "https://unsplash.com/ko/%EC%82%AC%EC%A7%84/%EB%B0%A4-%EC%8B%9C%EA%B0%84%EC%97%90-%EA%B1%B0%EB%A6%AC%EB%A5%BC-%EA%B1%B7%EB%8A%94-%EC%82%AC%EB%9E%8C%EB%93%A4-YF7yruF3W5E",
+    img3: "https://unsplash.com/ko/%EC%82%AC%EC%A7%84/%EA%B0%88%EC%83%89-%EB%82%98%EB%AC%B4-%ED%8F%AD%ED%92%8D-%EC%9C%A0%EB%A6%AC-%EB%AC%B8-6nuz52vsbWc"
+}
