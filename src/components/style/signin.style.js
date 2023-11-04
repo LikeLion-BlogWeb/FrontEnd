88eab0e49d794a626e4622ad03536539102edb95
PANdas <krsy0411@naver.com>
2023-11-04 22:45:37 +0900

    로그인 페이지 뼈대 완성 & 스타일링 완성 &  인증 로직 구현 필요

---
+++ src/components/style/signin.style.js
@@ -0,0 +1,76 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export const StyledForm = styled.form`
+    margin-top: 10vh;
+    min-height: 70vh;
+    margin: 0 auto;
+    max-width: 680px;
+    padding: 20px;
+    margin-top: 20px;
+    /* width는 내부 컴포넌트들의 사이즈로 결정 */
+`
+export const LoginTitle = styled.h1`
+    text-align: center;
+    margin-bottom: 4px;
+`
+
+export const FormInnerWrapper = styled.div`
+    margin-top: 20px;
+    width: 100%;
+`
+
+export const StyledLabel = styled.label`
+    display: block;
+    font-weight: 600;
+    margin-bottom: 10px;
+    margin-top: 20px;
+`
+
+export const LoginInput = styled.input`
+    height: 20px;
+    padding: 10px;
+    font-size: 18px;
+    border-radius: 0.3rem;
+    border: 1px solid orange;
+    width: 96%;
+    max-width: 680px;
+`
+
+export const ErrorMsgContainer = styled.div`
+    color: red;
+    font-weight: 600;
+`
+
+export const StyledLink = styled(Link)`
+    margin-left: 10px;
+    text-decoration: none;
+    color: black;
+    font-size: 20px;
+    font-weight: 600;
+
+    &:hover, &:focus {
+        color: orange;
+    }
+`
+
+export const LoginSubmitInput = styled.input`
+    width: 100%;
+    height: 48px;
+    font-weight: 600;
+    padding: 10px;
+    cursor: pointer;
+    margin: 0 auto;
+    font-size: 18px;
+    color: white;
+    background-color: black;
+
+
+    &:focus, &:hover {
+        background-color: orange;
+    }
+    /* 버튼이 클릭되었다가 떼질 때 */
+    &:active {
+        background-color: orangered;
+    }
+`
