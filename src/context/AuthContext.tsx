3d044419fa53a9ff62176e63e66435ad34c855fa
PANdas <krsy0411@naver.com>
2023-12-26 15:16:58 +0900

    Context api 생성:

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,27 @@
+import { useState, createContext } from "react"
+import { UserAuthProps } from "../types/user.context"
+
+
+interface authContextValue {
+    authToken: string;
+    setAuthToken: any;
+}
+
+// 초기값 설정
+export const AuthContext = createContext<authContextValue>({
+    authToken: "",
+    setAuthToken: ""
+});
+
+// 상태관리
+export function AuthContextProvider({children}: UserAuthProps) {
+    const [authToken, setAuthToken] = useState<any>({});
+
+    return (
+        <>
+            <AuthContext.Provider value={{authToken, setAuthToken}}>
+                { children }
+            </AuthContext.Provider>
+        </>
+    )
+}
