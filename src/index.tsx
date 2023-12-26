78c07c08bfc84195fbbf17d5081cba8245f6b9be
PANdas <krsy0411@naver.com>
2023-12-26 15:14:39 +0900

    기본설정 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,17 @@
-// react
-import React from 'react';
 // reactDOM
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <AuthContextProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+  </AuthContextProvider>
 );
