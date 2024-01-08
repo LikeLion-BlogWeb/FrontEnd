95a879db9a59d85f23d2a5d8beb16087029968ff
PANdas <krsy0411@naver.com>
2024-01-08 23:31:47 +0900

    우선, 유저 이메일을 전역상태변수로 올리기

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -5,21 +5,26 @@ import { UserAuthProps } from "../types/user.context"
 interface authContextValue {
     authToken: string;
     setAuthToken: any;
+    userEmail: string;
+    setUserEmail: any;
 }
 
 // 초기값 설정
 export const AuthContext = createContext<authContextValue>({
     authToken: "",
-    setAuthToken: ""
+    setAuthToken: "",
+    userEmail: "",
+    setUserEmail: "",
 });
 
 // 상태관리
 export function AuthContextProvider({children}: UserAuthProps) {
     const [authToken, setAuthToken] = useState<any>({});
-    
+    const [userEmail, setUserEmail] = useState("");
+
     return (
         <>
-            <AuthContext.Provider value={{authToken, setAuthToken}}>
+            <AuthContext.Provider value={{authToken, setAuthToken, userEmail, setUserEmail}}>
                 { children }
             </AuthContext.Provider>
         </>
