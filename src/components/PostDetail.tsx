da53ad1420cde837d25132f82ff5f10a18df40b6
PANdas <krsy0411@naver.com>
2024-01-09 09:07:52 +0900

    상세 게시물 페이지 구현

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,30 +1,56 @@
 import { AuthContext } from "context/AuthContext";
 import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import { PostProps } from "types/postlist.type";
 import { BACK_URL } from "../url";
-import { PostDetailContainer } from "./style/postdetail.style";
+import { PostAuthorName, PostCategory, PostDate, PostDelete, PostDetailContainer, PostDetailWrapper, PostEdit, PostEditLink, PostProfile, PostProfileWrapper, PostTextWrapper, PostUtilsWrapper } from "./style/postdetail.style";
+import { toast } from "react-toastify";
+import { PostTitle } from "./style/postlist.style";
 
 export default function PostDetail() {
     const [post, setPost] = useState<PostProps | null>(null);
+    // context api에서의 value는 authToken, setAuthToken 두가지가 있음
+    // 객체구조분해를 통해 authToken만 변수로 꺼내와서 사용
     const { authToken } = useContext(AuthContext);
+    // postDetail의 param은 post의 id
     const params = useParams();
+    const navigate = useNavigate();
+    const user_email = localStorage.getItem("user-email");
     
     // id기반으로 서버로부터 데이터 얻어냅니다
     async function getPost(id: string) {
         if(id) {
-            const response = await fetch(`${BACK_URL}/post/${id}`);
+            const response = await fetch(`//${BACK_URL}/post/${id}`, {
+                method: "GET",
+                headers: {
+                    "Authorization": authToken
+                }
+            });
             const postData = await response.json();
 
             setPost(postData);
         }
     }
 
-    async function deletePost() {
+    async function deletePost(id: any) {
         const checkConfirmBeforeDelete = window.confirm('정말 지우시겠습니까?');
 
+        // 사용자가 삭제를 결정하고, 게시물이 존재하면 삭제 프로세스 시작
         if(checkConfirmBeforeDelete && post && post.id) {
+            const response = await fetch(`//${BACK_URL}/post/${id}`, {
+                method: "DELETE",
+                headers: {
+                    "Authorization": authToken
+                }
+            });
 
+            if(response.status === 200) {
+                toast.success("게시글을 삭제 완료했습니다.");
+                // PostList로 돌아가기
+                navigate("/");
+            } else {
+                toast.error("게시글 삭제를 실패했습니다.");
+            }
         }
     }
     
@@ -37,9 +63,42 @@ export default function PostDetail() {
 
     return (
         <>
-            <h1>This is post-detail page</h1>
             <PostDetailContainer>
-
+                {
+                    post && (
+                        <>
+                            <PostDetailWrapper>
+                                <PostTitle>{post?.title}</PostTitle>
+                                <PostProfileWrapper>
+                                    <PostProfile />
+                                    <PostAuthorName>{post?.email}</PostAuthorName>
+                                    <PostDate>{post?.writeDate}</PostDate>
+                                </PostProfileWrapper>
+                                <PostUtilsWrapper>
+                                    <PostCategory>
+                                        {/* 추후에 추가될 수 있는 부분 */}
+                                        자유주제
+                                    </PostCategory>
+                                    {/* 작성자 이메일과 동일하면 삭제 아이콘도 뜨도록 설정 */}
+                                    {   
+                                        post?.email === user_email && (
+                                            <>
+                                                <PostDelete role="presentation" onClick={() => deletePost(params)}>삭제</PostDelete>
+                                                <PostEdit>
+                                                    <PostEditLink to={`/`}>수정</PostEditLink>
+                                                </PostEdit>
+                                            </>
+                                        ) 
+                                    }
+                                    <PostTextWrapper>
+                                        {post?.content}
+                                    </PostTextWrapper>
+                                </PostUtilsWrapper>
+                                {/* 댓글 기능? */}
+                            </PostDetailWrapper>
+                        </>
+                    )
+                }
             </PostDetailContainer>
         </>
     )
--- src/components/style/postdetail.style.js
+++ src/components/style/postdetail.style.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 export const PostDetailContainer = styled.div`
@@ -9,12 +10,85 @@ export const PostDetailContainer = styled.div`
     /* 가로 중앙 정렬 */
     margin: 0 auto;
     line-height: 24px;
+
+    border-left: 1px solid orange;
+    border-right: 1px solid orange;
+`
+
+export const PostDetailWrapper = styled.div`
+    /* 세로 가로 */
+    padding: 24px 0;
+    border-top: 1px solid #f2f2f2;
 `
 
 export const PostTitle = styled.div`
-
+    font-size: 20px;
+    font-weight: 600;
+    /* 세로 가로 */
+    margin: 14px 0px;
 `
 
 export const PostProfileWrapper = styled.div`
+    /* 세로 가로 */
+    padding: 10px 0;
+`
+export const PostProfile = styled.div`
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+    background: #f2f2f2;
+`
+export const PostAuthorName = styled.div`
+    color: gray;
+`
+export const PostDate = styled.div`
+    color: gray;
+`
 
+export const PostUtilsWrapper = styled.div`
+    display: flex;
+    gap: 8px;
+    flex-direction: column;
+    font-size: 14px;
+    color: gray;
+`
+export const PostCategory = styled.div`
+    color: gray;
+    font-size: 12px;
+    border: 1px solid lightgray;
+    border-radius: 10px;
+    background-color: #f2f2f2;
+    padding: 0px 4px;
+    /* 부모 너비 갖지 않도로 설정 */
+    width: fit-content;
+`
+
+export const PostDelete = styled.div`
+    cursor: pointer;
+    color: white;
+
+    &:hover, &:focus {
+        color: black;
+    }
+`
+
+export const PostEdit = styled.div`
+    &:hover, &:focus {
+        color: black;
+    }
+`
+export const PostEditLink = styled(Link)`
+    text-decoration: none;
+    color: gray;
+
+    &:hover, &:focus {
+        color: black;
+    }
+`
+
+export const PostTextWrapper = styled.div`
+    color: dimgray;
+    font-size: 16px;
+    padding: 20px 0;
+    white-space: pre-wrap;
 `
