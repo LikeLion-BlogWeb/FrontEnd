33ac932109a90c7fe371e3c2bc5e1fba8e9f409a
minzzn <188min.kk@gmail.com>
2024-02-05 17:16:54 +0900

    Feat: 로그인 버튼 추가

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,8 +1,13 @@
-import { HeaderLink, HeaderLogoLink, StyledHeader } from "../style/header.style";
+import { HeaderLink, HeaderLoginLink, HeaderLogoLink, StyledHeader } from "../style/header.style";
+import { useContext, useState } from "react";
+import { AuthContext } from "context/AuthContext";
 
 export default function Header({
     linkTextDisplay = false
     }: { linkTextDisplay: boolean }) {
+    const { authToken, userEmail } = useContext(AuthContext);
+    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
     return (
         <StyledHeader>
             <HeaderLogoLink to="/">
@@ -12,6 +17,8 @@ export default function Header({
                 <HeaderLink to="/posts/new" displayattr={linkTextDisplay ? "true" : "false"}>글쓰기</HeaderLink>
                 <HeaderLink to="/posts" displayattr={linkTextDisplay ? "true" : "false"}>게시물</HeaderLink>
                 <HeaderLink to="/profile" displayattr={linkTextDisplay ? "true" : "false"}>프로필</HeaderLink>
+                <HeaderLoginLink to="/signin" displayattr={linkTextDisplay ? "true" : "false"}>로그인</HeaderLoginLink>
+                {/* home 화면과 login 화면의 접근제한을 바꾸어야 토큰이 날아가서 작동하는게 보일 듯 */}
             </div>
         </StyledHeader>
     )
