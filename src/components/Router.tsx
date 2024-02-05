50cfa4ba6aed6676b55d6d257560f8de50b5cda4
minzzn <188min.kk@gmail.com>
2024-02-05 18:41:02 +0900

    chore: review에 따른 코드와 무관한 중괄호 제거

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -13,27 +13,25 @@ export default function Router() {
     return (
         <>
             <Routes>
-                {
-                    <>
-                        {/* 진입 지점 */}
-                        <Route path="/" element={<Home />} />
-                        {/* 게시물 리스트 볼 수 있는 페이지 */}
-                        <Route path="/posts" element={<List />} />
-                        {/* 로그인 후 좀 더 디테일한 게시물 목록 페이지? */}
-                        <Route path="/posts/:id" element={<Detail />} />
-                        {/* 글쓰기 부분 */}
-                        <Route path="/posts/new" element={<New />} />
-                        {/* 게시물 편집 */}
-                        <Route path="/posts/edit/:id" element={<Edit />} />
-                        {/* 프로필 확인 페이지 */}
-                        <Route path="/profile" element={<ProfileView />} />
-                        {/* default 경로 설정 */}
-                        <Route path="*" element={<Navigate replace to="/" />} />
-                        {/* 회원가입 + 로그인 페이지 경로 설정 */}
-                        <Route path="/signin" element={<Signin />} />
-                        <Route path="/signup" element={<Signup />} />
-                    </>
-                }
+                <>
+                    {/* 진입 지점 */}
+                    <Route path="/" element={<Home />} />
+                    {/* 게시물 리스트 볼 수 있는 페이지 */}
+                    <Route path="/posts" element={<List />} />
+                    {/* 로그인 후 좀 더 디테일한 게시물 목록 페이지? */}
+                    <Route path="/posts/:id" element={<Detail />} />
+                    {/* 글쓰기 부분 */}
+                    <Route path="/posts/new" element={<New />} />
+                    {/* 게시물 편집 */}
+                    <Route path="/posts/edit/:id" element={<Edit />} />
+                    {/* 프로필 확인 페이지 */}
+                    <Route path="/profile" element={<ProfileView />} />
+                    {/* default 경로 설정 */}
+                    <Route path="*" element={<Navigate replace to="/" />} />
+                    {/* 회원가입 + 로그인 페이지 경로 설정 */}
+                    <Route path="/signin" element={<Signin />} />
+                    <Route path="/signup" element={<Signup />} />
+                </>
             </Routes>
         </>
     )
