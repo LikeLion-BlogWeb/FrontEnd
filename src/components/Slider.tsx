4ee03d73e7516c8698e0f9782e3698a493bfb1a2
PANdas <krsy0411@naver.com>
2024-03-03 23:32:25 +0900

    [Refactoring] slider컴포넌트 동적으로 만들기 1차작업 완료

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,75 +1,51 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import * as Styled from "./style/slider.style";
-import likelion1 from "../img/likelion_1.png";
-import likelion2 from "../img/likelion_2.jpeg";
-import likelion3 from "../img/likelion_3.jpeg";
-
+// images는 배열입니다
+import { images } from "constant/constant";
 
 export default function Slider() {
-    const [activeImg, setActiveImg] = useState(1);
+    const [activeImg, setActiveImg] = useState<number>(0);
 
     return (
         <>
             <Styled.SlidesContainer>
-                {/* ul안에 li태그 3묶음이 존재합니다. 각 li태그 안에는 이미지컨테이너와 화살표 두개를 지닌 컨테이너 두 개가 있습니다. */}
+                {/* ul태그 내에서 각 li태그 안에는 이미지컨테이너와 화살표 두개를 지닌 컨테이너 두 개가 있습니다. */}
                 <Styled.SlidersWrapperUl>
-                    <Styled.SliderInput id="img-1" checked={activeImg === 1} />
-                    <Styled.SliderLi className="one-slide-container">
-                        {/* 호버시에 상태를 하나 바꿔서 자식 컴포넌트들이 이를 토대로 opacity를 변경할 수 있도록 */}
-                        <Styled.OneSlideImgContainer className="slide-img-container">
-                            <Styled.SlideImg alt="img1" src={likelion1} id="img-1" />
-                        </Styled.OneSlideImgContainer>
-                        <Styled.SlideControllContainer className="slide-controls">
-                            <Styled.ControlLabel onClick={()=>setActiveImg(3)} direction="left">
-                                {/* left 화살표 */}
-                                <span>&lsaquo;</span>
-                            </Styled.ControlLabel>
-                            <Styled.ControlLabel onClick={()=>setActiveImg(2)} direction="right">
-                                {/* right 화살표 */}
-                                <span>&rsaquo;</span>
-                            </Styled.ControlLabel>
-                        </Styled.SlideControllContainer>
-                    </Styled.SliderLi>
+                    {
+                        images.map((imgSrc, index) => {
 
-                    <Styled.SliderInput id="img-2" checked={activeImg === 2} />
-                    <Styled.SliderLi className="one-slide-container">
-                        {/* 호버시에 상태를 하나 바꿔서 자식 컴포넌트들이 이를 토대로 opacity를 변경할 수 있도록 */}
-                        <Styled.OneSlideImgContainer className="slide-img-container">
-                            <Styled.SlideImg alt="img2" src={likelion2} id="img-2" />
-                        </Styled.OneSlideImgContainer>
-                        <Styled.SlideControllContainer className="slide-controls">
-                            <Styled.ControlLabel onClick={()=>setActiveImg(1)} direction="left">
-                                {/* left 화살표 */}
-                                <span>&lsaquo;</span>
-                            </Styled.ControlLabel>
-                            <Styled.ControlLabel onClick={()=>setActiveImg(3)} direction="right">
-                                {/* right 화살표 */}
-                                <span>&rsaquo;</span>
-                            </Styled.ControlLabel>
-                        </Styled.SlideControllContainer>
-                    </Styled.SliderLi>
-
-                    <Styled.SliderInput id="img-3" checked={activeImg === 3} />
-                    <Styled.SliderLi className="one-slide-container">
-                        {/* 호버시에 상태를 하나 바꿔서 자식 컴포넌트들이 이를 토대로 opacity를 변경할 수 있도록 */}
-                        <Styled.OneSlideImgContainer className="slide-img-container">
-                            <Styled.SlideImg alt="img3" src={likelion3} id="img-3" />
-                        </Styled.OneSlideImgContainer>
-                        <Styled.SlideControllContainer className="slide-controls">
-                            <Styled.ControlLabel onClick={()=>setActiveImg(2)} direction="left">
-                                {/* left 화살표 */}
-                                <span>&lsaquo;</span>
-                            </Styled.ControlLabel>
-                            <Styled.ControlLabel onClick={()=>setActiveImg(1)} direction="right">
-                                {/* right 화살표 */}
-                                <span>&rsaquo;</span>
-                            </Styled.ControlLabel>
-                        </Styled.SlideControllContainer>
-                    </Styled.SliderLi>
+                            return (
+                                <Fragment key={index}>
+                                    <Styled.SliderInput id={`img-${index}`} checked={activeImg === index} />
+                                    <Styled.SliderLi className="one-slide-container" >
+                                        {/* 호버시에 상태를 하나 바꿔서 자식 컴포넌트들이 이를 토대로 opacity를 변경할 수 있도록 */}
+                                        <Styled.OneSlideImgContainer className="slide-img-container">
+                                            <Styled.SlideImg alt={`img${index}`} src={imgSrc} id={`img-${index}`} />
+                                        </Styled.OneSlideImgContainer>
+                                        <Styled.SlideControllContainer className="slide-controls">
+                                            <Styled.ControlLabel onClick={() => setActiveImg(index-1 < 0 ? images.length-1 : index-1)} direction="left">
+                                                {/* left 화살표 */}
+                                                <span>&lsaquo;</span>
+                                            </Styled.ControlLabel>
+                                            <Styled.ControlLabel onClick={()=>setActiveImg(index+1 > images.length-1 ? 0 : index+1)} direction="right">
+                                                {/* right 화살표 */}
+                                                <span>&rsaquo;</span>
+                                            </Styled.ControlLabel>
+                                        </Styled.SlideControllContainer>
+                                    </Styled.SliderLi>
+                                </Fragment>
+                            )
+                        })
+                    }
                     <Styled.SlidesDots className="slide_dots">
-                        <Styled.SlidesDot onClick={()=>setActiveImg(1)} id="img-dot-1"></Styled.SlidesDot>
-                        <Styled.SlidesDot onClick={()=>setActiveImg(2)} id="img-dot-2"></Styled.SlidesDot>
-                        <Styled.SlidesDot onClick={()=>setActiveImg(3)} id="img-dot-3"></Styled.SlidesDot>
+                        {
+                            images.map((_, index) => {
+
+                                return (
+                                    <Styled.SlidesDot id={`img-dot-${index}`} onClick={() => setActiveImg(index)} key={index}></Styled.SlidesDot>
+                                )
+                            })
+                        }
                     </Styled.SlidesDots>
                 </Styled.SlidersWrapperUl>
             </Styled.SlidesContainer>
